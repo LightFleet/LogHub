440483181aa1d34304a763a2d6be4671d503a724
Dmitry Novik <dmitry.novik@eleving.com>
2023-03-07 20:33:39 +0200

    clean, dockerized Yii2 app initialized

---
+++ controllers/SiteController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+
+class SiteController extends Controller
+{
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
---
+++ views/site/index.php
@@ -0,0 +1,20 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'LogHub';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent">
+    </div>
+
+    <div class="body-content">
+        Welcome to LogHub!
+
+        <br>
+        <br>
+        <p>This app is designed for a single purpose: </p>
+
+    </div>
+</div>
